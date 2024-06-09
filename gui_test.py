099cd1b59b649cf838a7e161cc77ebde4381492f
cflixdev-projects <cflixdev@proton.me>
2024-06-09 16:10:51 +0200

    offset

--- gui_test.py
+++ gui_test.py
@@ -1,15 +1,16 @@
 import sys
-import customtkinter
+import customtkinter as ctk
 import keyboard
 from threading import Thread, Event
-from main import start
+import pyautogui
+from main import start, offset_y, offset_x
 
-customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
-customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+ctk.set_appearance_mode("System")  # Modes: system (default), light, dark
+ctk.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
-app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-app.geometry("300x150")
-customtkinter.set_default_color_theme('dark-blue')
+app = ctk.CTk()  # create CTk window like you do with the Tk window
+app.geometry("350x150")
+ctk.set_default_color_theme('dark-blue')
 
 app.resizable(False, False)
 app.title('KlickBot')
@@ -20,30 +21,61 @@ def update_status(status):
     status_label.configure(text=status)
     app.update()  # Force GUI update
 
+def updateCoordinates():
+    selected_index = print_selected_value()
+    global offset_x, offset_y
+    if selected_index == 'Laptop - 15.6 Zoll':
+        offset_x += 0
+        offset_y += 0
+    elif selected_index == '24 Zoll (Standard)':
+        offset_x += 50
+        offset_y += 70
+    elif selected_index == '27 Zoll':
+        offset_x += 480
+        offset_y += 490
+    print("Updated coordinates: x =", offset_x, ", y =", offset_y)
+    return offset_x, offset_y
+
 def startVisual():
     update_status("Loading...")  # Show 'Loading...' when KlickBot starts
-    # Here you add the code that starts the KlickBot
-    t = Thread(target=run_start)  # Run start() in a separate thread
+    t = Thread(target=run_start, args=(offset_x, offset_y))  # Pass the offsets as arguments
     t.start()
+# move mouse with force to the top left corner to stop the bot
 
-def run_start():
-    start()  # Your start function here
-    if not stop_event.is_set():
-        update_status("Done")  # Update status to 'Done' after KlickBot finishes
-
+def run_start(offset_x, offset_y):
+    try:
+        start(offset_x, offset_y)  # Your start function here
+        if not stop_event.is_set():
+            update_status("Done")  # Update status to 'Done' after KlickBot finishes
+    except pyautogui.FailSafeException:
+        update_status("Run Again")
 def on_escape(event=None):
     stop_event.set()  # Signal the thread to stop
     sys.exit()  # Exit the application
 
+def print_selected_value():
+    selected_index = combo.current()
+    print("Selected index:", selected_index)
+    return selected_index
+
+
+
+# items = ('Laptop - 15.6 Zoll', '24 Zoll (Standard)', '27 Zoll' )
+combo = ctk.CTkComboBox(app, width=150, values=['Laptop - 15.6 Zoll', '24 Zoll (Standard)', '27 Zoll'], corner_radius=8, state='readonly')
+combo.place(rely=0.2, relx=0.5, anchor=ctk.CENTER)
+combo.set('choose Screen')
+
+
 # Use CTkButton instead of tkinter Button
-upperText = customtkinter.CTkLabel(master=app, text='Startet nach 3s')
-upperText.place(relx=0.5, rely=0.3, anchor=customtkinter.CENTER)
+upperText = ctk.CTkLabel(master=app, text='Startet nach 3s')
+upperText.place(relx=0.5, rely=0.4, anchor=ctk.CENTER)
 
-button = customtkinter.CTkButton(master=app, text="Starte KlickBot", command=startVisual)
-button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+button = ctk.CTkButton(master=app, text="Starte KlickBot", command=startVisual)
+button.place(relx=0.5, rely=0.6, anchor=ctk.CENTER)
 
-status_label = customtkinter.CTkLabel(master=app, text='', text_color='white')  # Empty label for displaying status
-status_label.place(relx=0.5, rely=0.7, anchor=customtkinter.CENTER)
+status_label = ctk.CTkLabel(master=app, text='', text_color='white')  # Empty label for displaying status
+status_label.place(relx=0.5, rely=0.8, anchor=ctk.CENTER)
 app.bind("<Escape>", on_escape)  # Bind Escape key to on_escape function
+
 
 app.mainloop()
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
+import sys
+import multiprocessing
 import pyautogui
 import time
 import keyboard
-
 '''
 1920 x 1080
 my laptop
@@ -34,60 +36,64 @@ def scroll(sValue):
 
     pyautogui.scroll(sValue)
 
-def start():
+
+offset_x = 0
+offset_y = 0
+def start(offset_x, offset_y):
+
     x = 0.5
     time.sleep(3)
     # move to lumtri farben
-    pyautogui.moveTo(x=641, y=115, )
+    pyautogui.moveTo(x=641 + offset_x, y=115 + offset_y, )
     pyautogui.click()
     # click lut
-    pyautogui.moveTo(x=633, y=308,)
+    pyautogui.moveTo(x=633+ offset_x, y=308+ offset_y,)
     pyautogui.click()
     # choose lut
-    pyautogui.moveTo(x=690, y=471,)
+    pyautogui.moveTo(x=690+ offset_x, y=471+ offset_y,)
     pyautogui.click()
     # klick automatic
-    pyautogui.moveTo(x = 1176, y= 357,)
+    pyautogui.moveTo(x = 1176+ offset_x, y= 357+ offset_y,)
     pyautogui.click()
     time.sleep(1)
     # move to scroll bar, scroll down
-    pyautogui.mouseDown(button='left', x=1372, y=322,)
+    pyautogui.mouseDown(button='left', x=1372, y=322+ offset_y,)
     # runter bringen
-    pyautogui.moveTo(x=1372, y=565,)
+    pyautogui.moveTo(x=1372+ offset_x, y=565+ offset_y,)
     # lösen
-    pyautogui.mouseUp(button='left', x=1372, y=565)
+    pyautogui.mouseUp(button='left', x=1372+ offset_x, y=565+ offset_y)
 
     # klick kreativ
-    pyautogui.moveTo(x=472, y=426, )
+    pyautogui.moveTo(x=472+ offset_x, y=426, )
     pyautogui.click()
 
     # kreativ - scharf, dnymaik, sättigung
-    pyautogui.moveTo(x=1318, y=549, duration=x)
+    pyautogui.moveTo(x=1318+ offset_x, y=549+ offset_y, duration=x)
     pyautogui.click()
     pyautogui.write('4')
 
-    pyautogui.moveTo(x=1321, y=584, duration=x)
+    pyautogui.moveTo(x=1321+ offset_x, y=584+ offset_y, duration=x)
     pyautogui.click()
     pyautogui.write('8')
 
-    pyautogui.moveTo(x=1325, y=613, duration=x)
+    pyautogui.moveTo(x=1325+ offset_x, y=613+ offset_y, duration=x)
     pyautogui.click()
     pyautogui.write('104')
     pyautogui.press('enter')
 
     pyautogui.scroll(5000)
 
-    pyautogui.moveTo( x = 643, y= 359,duration=x)
+    pyautogui.moveTo( x = 643+ offset_x, y= 359+ offset_y,duration=x)
     pyautogui.click()
-    pyautogui.moveTo(y= 610,duration=x)
+    pyautogui.moveTo(y= 610+ offset_y,duration=x)
 
 
-    pyautogui.mouseDown(button='left', x=880, y=465,)
+    pyautogui.mouseDown(button='left', x=880+ offset_x, y=465+ offset_y,)
     # runter bringen
-    pyautogui.moveTo(x=880, y=798,)
+    pyautogui.moveTo(x=880+ offset_x, y=798+ offset_y,)
     # lösen
 
-    pyautogui.mouseUp(button='left', x=880, y=798)
+    pyautogui.mouseUp(button='left', x=880+ offset_x, y=798+ offset_y)
 
     '''
     dialog vorgabe muss leer sein
@@ -98,34 +104,34 @@ def start():
     # audio
 
     # essential sound
-    pyautogui.moveTo(x=955, y=116, duration=x)
+    pyautogui.moveTo(x=955+ offset_x, y=116+ offset_y, duration=x)
     pyautogui.click()
     #vorgabe
-    pyautogui.moveTo(x=868, y=240, duration=x)
+    pyautogui.moveTo(x=868+ offset_x, y=240+ offset_y, duration=x)
     pyautogui.click()
     # move to dialog
-    pyautogui.moveTo(x=625, y=262, duration=x)
+    pyautogui.moveTo(x=625+ offset_x, y=262+ offset_y, duration=x)
     pyautogui.click()
     # dialog -> dialog mit rauschen
-    pyautogui.moveTo(x=783, y=422, duration=x)
+    pyautogui.moveTo(x=783+ offset_x, y=422+ offset_y, duration=x)
     pyautogui.click()
 
     # sprache verbessern
-    pyautogui.moveTo(x=489, y=331, duration=x)
+    pyautogui.moveTo(x=489+ offset_x, y=331+ offset_y, duration=x)
     pyautogui.click()
-    pyautogui.moveTo(x=489, y=371, duration=x)
+    pyautogui.moveTo(x=489+ offset_x, y=371+ offset_y, duration=x)
     pyautogui.click()
-    pyautogui.moveTo(x=1330, y=405, duration=x)
+    pyautogui.moveTo(x=1330+ offset_x, y=405+ offset_y, duration=x)
     pyautogui.click()
     pyautogui.write('3,5')
     pyautogui.press('enter')
 
     # reparieren
-    pyautogui.moveTo(x=489, y=331, duration=x)
+    pyautogui.moveTo(x=489+ offset_x, y=331  + offset_y, duration=x)
     pyautogui.click()
-    pyautogui.moveTo(x=503, y=428, duration=x)
+    pyautogui.moveTo(x=503+ offset_x, y=428+ offset_y , duration=x)
     pyautogui.click()
-    pyautogui.moveTo(x=1333, y=478, duration=x)
+    pyautogui.moveTo(x=1333+ offset_x, y=478+ offset_y, duration=x)
     pyautogui.click()
     pyautogui.write('3,5')
     pyautogui.press('enter')
@@ -135,15 +141,15 @@ def start():
     # geht mit maus nach links wohinmach scrollen kann
     scroll(-3000)
 
-    pyautogui.moveTo(x=1326, y=341, duration=x)
+    pyautogui.moveTo(x=1326+ offset_x, y=341+ offset_y, duration=x)
     pyautogui.click()
     pyautogui.write('4')
     pyautogui.press('enter')
 
-    pyautogui.moveTo(x=449, y=423, duration=x)
+    pyautogui.moveTo(x=449+ offset_x, y=423+ offset_y, duration=x)
     pyautogui.click()
 
-    pyautogui.moveTo(x=1331, y=420, duration=x)
+    pyautogui.moveTo(x=1331+ offset_x, y=420+ offset_y, duration=x)
     pyautogui.click()
     pyautogui.write('3,3')
     pyautogui.press('enter')
@@ -151,75 +157,95 @@ def start():
 
     scroll(-3000)
     # de ess
-    pyautogui.moveTo(x=1326, y=325, duration=x)
+    pyautogui.moveTo(x=1326+ offset_x, y=325+ offset_y, duration=x)
     pyautogui.click()
     pyautogui.write('3,8')
 
-    pyautogui.moveTo( x = 458, y= 396, duration = x)
+    pyautogui.moveTo( x = 458+ offset_x, y= 396+ offset_y, duration = x)
     pyautogui.click()
-    pyautogui.moveTo( x = 1326, y= 389, duration = x)
+    pyautogui.moveTo( x = 1326+ offset_x, y= 389+ offset_y, duration = x)
     pyautogui.click()
     pyautogui.write('3')
     pyautogui.press('enter')
 
     # klarkheit tiefe tonlage
     scroll(-3000)
-    pyautogui.moveTo(x=1326, y=440, duration=x)
+    pyautogui.moveTo(x=1326+ offset_x, y=440+ offset_y, duration=x)
     pyautogui.click()
-    pyautogui.moveTo( x = 1081, y= 438, duration = x)
+    pyautogui.moveTo( x = 1081+ offset_x, y= 438+ offset_y, duration = x)
     pyautogui.click()
 
-    pyautogui.moveTo( x = 458, y= 483, duration = x)
+    pyautogui.moveTo( x = 458+ offset_x, y= 483+ offset_y, duration = x)
     pyautogui.click()
-    pyautogui.moveTo(x=1320, y=482, duration=x)
+    pyautogui.moveTo(x=1320+ offset_x, y=482+ offset_y, duration=x)
     pyautogui.click()
     pyautogui.write('3,5')
     pyautogui.press('enter')
     scroll(-3000)
     #klarheit
-    pyautogui.moveTo(x=448, y=434, duration=x)
+    pyautogui.moveTo(x=448+ offset_x, y=434+ offset_y, duration=x)
     pyautogui.click()
 
-    pyautogui.moveTo(x=568, y=469, duration=x)
+    pyautogui.moveTo(x=568+ offset_x, y=469+ offset_y, duration=x)
     pyautogui.click()
 
-    pyautogui.moveTo(x=581, y=655, duration=x)
+    pyautogui.moveTo(x=581+ offset_x, y=655+ offset_y, duration=x)
     pyautogui.click()
     scroll(-3000)
     #stimmlage
-    pyautogui.moveTo( x = 451, y= 479, duration = x)
+    pyautogui.moveTo( x = 451+ offset_x, y= 479+ offset_y, duration = x)
     pyautogui.click()
-    pyautogui.moveTo( x = 630, y= 510, duration = x)
+    pyautogui.moveTo( x = 630+ offset_x, y= 510+ offset_y, duration = x)
     pyautogui.click()
 
     scroll(-3000)
 
     # klarheit
-    pyautogui.moveTo(x=1330, y=496, duration=x)
+    pyautogui.moveTo(x=1330+ offset_x, y=496+ offset_y, duration=x)
     pyautogui.click()
-    pyautogui.moveTo( x = 559, y= 493, duration = x)
+    pyautogui.moveTo( x = 559+ offset_x, y= 493+ offset_y, duration = x)
     pyautogui.click()
     scroll(-3000)
 
-    pyautogui.moveTo(x=544, y=404, duration=x)
+    pyautogui.moveTo(x=544+ offset_x, y=404+ offset_y, duration=x)
     pyautogui.click()
-    pyautogui.moveTo(x=554, y=630, duration=x)
+    pyautogui.moveTo(x=554+ offset_x, y=630+ offset_y, duration=x)
     pyautogui.click()
-    pyautogui.moveTo(x=1297, y=459, duration=x)
+    pyautogui.moveTo(x=1297+ offset_x, y=459+ offset_y, duration=x)
     pyautogui.click()
     pyautogui.write('2,5')
     pyautogui.press('enter')
 
-
-'''
+'''k
     effekte = umschalt + 7 damit das effekt fenster/arbeitsbereich sich links öffnet
     '''
 
 
+# multiprocessing
+
+
+# p2 = multiprocessing.Process(target=start)
+
+
 if __name__ == '__main__':
+
 
     screen_width, screen_height = pyautogui.size()
 
+
     print("Screen width:", screen_width)
     print("Screen height:", screen_height)
-    start()
+    start(offset_x,offset_y)
+    # p1.start()
+    # p2.start()
+
+
+    # p1.join()
+    # p2.join()
+
+# finish  = time.perf_counter()
+# print("Finished running after seconds : ", finish)
+#
+
+
+
